b498f34c5ef6fe8d28348869d4efa68ac2de2cb8
German <german.estefan81@gmail.com>
2022-03-22 15:58:57 -0300

    Front end fixed

--- controllers/urlShorted.ts
+++ controllers/urlShorted.ts
@@ -6,7 +6,7 @@ export const saveUrlShorted = async (req: Request, res: Response) => {
 
     const { url } = req.body;
     //Verificamos que la URL sea valida
-    console.log(url)
+    console.log(req.body)
     const isValidUrl = /^https?:\/\/\w/gi.test(url);
     if (!isValidUrl) return res.json({ error: 'Invalid URL' });
 
--- dist/controllers/urlShorted.js
+++ dist/controllers/urlShorted.js
@@ -15,7 +15,7 @@ const Url_1 = require("../models/Url");
 const saveUrlShorted = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { url } = req.body;
     //Verificamos que la URL sea valida
-    console.log(url);
+    console.log(req.body);
     const isValidUrl = /^https?:\/\/\w/gi.test(url);
     if (!isValidUrl)
         return res.json({ error: 'Invalid URL' });
